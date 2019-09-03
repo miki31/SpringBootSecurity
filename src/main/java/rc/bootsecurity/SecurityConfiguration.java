1e288acb51cf5ddde417ad5bfc245a6361228037
miki31 <it.misha2016@gmail.com>
2019-09-03 20:32:47 +0300

    [Spring Boot Security] #12 Configure Role Based Authorization change SecurityConfiguration and get permissions to users with roles

--- src/main/java/rc/bootsecurity/SecurityConfiguration.java
+++ src/main/java/rc/bootsecurity/SecurityConfiguration.java
@@ -18,19 +18,27 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth
                 .inMemoryAuthentication()
                 .withUser("admin")
-                    .password(passwordEncoder().encode("user123"))
+                    .password(passwordEncoder().encode("admin123"))
                     .roles("ADMIN")
                 .and()
                 .withUser("misha")
                     .password(passwordEncoder().encode("misha123"))
-                    .roles("USER");
+                    .roles("USER")
+                .and()
+                .withUser("manager")
+                    .password(passwordEncoder().encode("manager123"))
+                    .roles("MANAGER");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()     // access to all functions without permission
+                .antMatchers("/index.html").permitAll()
+                .antMatchers("/profile/**").authenticated()  // "/profile/index" --> can connect to that file;   "/profile/**" --> can connect to all files in folder "profile"
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .antMatchers("/management/**").hasAnyRole("ADMIN", "MANAGER")
                 .and()
                 .httpBasic();
     }
