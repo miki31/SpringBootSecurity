b676eaec8670ae85b948c1f8f4afef2df28f5a29
miki31 <it.misha2016@gmail.com>
2019-09-03 16:16:12 +0300

    [Spring Boot Security] #08 Unsecured Spring Boot App Demo Start project add annotation to run project in Spring @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})

--- src/main/java/rc/bootsecurity/BootSecurityApplication.java
+++ src/main/java/rc/bootsecurity/BootSecurityApplication.java
@@ -1,7 +1,10 @@
 package rc.bootsecurity;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class BootSecurityApplication {
 
     public static void main(String[] args) {
